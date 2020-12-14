ade1b5f5e508efb13623b23ee9a4d58577c41501
abrar <9529693800aa@gmail.com>
2020-12-14 20:52:32 +0530

    Search field imp;emented succesfully

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react'
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
 
 import './App.css';
 
-class App extends Component  {
-  constructor(){
-    super();
-    this.state={
-     monsters:[]
-    }
-  }
-  componentDidMount(){
-    fetch(' http://jsonplaceholder.typicode.com/users').then(response=>response.json()).then(data=>this.setState({
-      monsters:data
-    }))
-  }
-  render(){
-  return (
-    <div className="App">
-        
-       <CardList monsters={this.state.monsters}/>
 
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            monsters: [],
+            searchField:'',
+        }
+    }
+    componentDidMount() {
+        fetch(' http://jsonplaceholder.typicode.com/users').then(response => response.json())
+        .then(data => {
+              this.setState({monsters: data})
               
-    </div>
-  );
-}
+      })
+ 
+          
+    }
+    render() {
+        const {monsters,searchField}=this.state
+        let filteredMonsters=monsters
+        if (this.state.searchField){
+          console.log('searchField=',searchField)
+          console.log('monsters=',monsters)
+          filteredMonsters=monsters.filter(monster=>{
+            return monster.name.toLowerCase().includes(searchField.toLowerCase())
+          })
+          console.log("filteredMonsters=",filteredMonsters)
+        }else{
+          filteredMonsters=this.state.monsters
+        }
+
+        return ( 
+          <div className = "App" >
+            <input type = "search" placeholder="search-monster" onChange={e=>this.setState({searchField:e.target.value}) } />
+            <CardList monsters = { filteredMonsters }/>
+
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import './card.style.css'
 
 export const Card=(props)=>{
 return(
-<div><h1>{props.monster.name}</h1></div>
+<div className='card-container'>
+	<img alt="monster" src={`https://robohash.org/${props.monster.name}?set=set2&size=180x180`}/>
+	<h2>{props.monster.name}</h2>
+	{props.monster.email}
+</div>
 )
 
 	}
